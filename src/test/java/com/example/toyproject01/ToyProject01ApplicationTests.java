1fa697116614e9267ddf11d1bd69ce1005dcf69e
yoo-hyunseung <hsung122@naver.com>
2024-03-01 17:53:14 +0900

    Initial commit

---
+++ src/test/java/com/example/toyproject01/ToyProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toyproject01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyProject01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
